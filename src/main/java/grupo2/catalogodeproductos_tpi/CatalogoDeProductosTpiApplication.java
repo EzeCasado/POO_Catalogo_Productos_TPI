9cfd94cdc2a1efc97e01e9939323da24cfd05a1e
EzeCasado <ezecasado2002@gmail.com>
2025-11-04 19:30:49 -0300

    Inicializacion del Proyecto

---
+++ src/main/java/grupo2/catalogodeproductos_tpi/CatalogoDeProductosTpiApplication.java
@@ -0,0 +1,13 @@
+package grupo2.catalogodeproductos_tpi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoDeProductosTpiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogoDeProductosTpiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/grupo2/catalogodeproductos_tpi/CatalogoDeProductosTpiApplicationTests.java
@@ -0,0 +1,13 @@
+package grupo2.catalogodeproductos_tpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoDeProductosTpiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
