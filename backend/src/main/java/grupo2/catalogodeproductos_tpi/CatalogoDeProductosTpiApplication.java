71f6120d5c70ce2e43598c85d07e6f303ce577f3
EzeCasado <ezecasado2002@gmail.com>
2025-11-15 10:02:45 -0300

    feat(product): Tarea 7 completada. Implementada lógica de búsqueda y filtros para GET /products

--- backend/src/main/java/grupo2/catalogodeproductos_tpi/CatalogoDeProductosTpiApplication.java
+++ backend/src/main/java/grupo2/catalogodeproductos_tpi/CatalogoDeProductosTpiApplication.java
@@ -2,8 +2,11 @@ package grupo2.catalogodeproductos_tpi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
 
 @SpringBootApplication
 public class CatalogoDeProductosTpiApplication {
@@ -13,8 +16,12 @@ public class CatalogoDeProductosTpiApplication {
     }
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+
+
+        return builder.setConnectTimeout(Duration.ofSeconds(3)) // 3 segundos para establecer una conexión
+                .setReadTimeout(Duration.ofSeconds(3)) // 3 segundos para recibir una respuesta
+                .build();
     }
 
 }
