e32db5bc42f231454825b5f929d4c084b01a737b
EzeCasado <ezecasado2002@gmail.com>
2025-12-01 10:48:11 -0300

    cambios finales y ajustes de URLS

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8082', // backend del módulo 2
+  baseURL: '', // backend del módulo 2
   headers: {
     'Content-Type': 'application/json',
   },
